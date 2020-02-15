558e997e71e2aa9d80f91423787c084f0ab2abc5
fzlink <fazlibozatay@gmail.com>
2020-02-15 19:10:50 +0300

    Procedural Terrain Generation Part 1(Sebastian Tut)

---
+++ My Little Perfect World/Assets/DragRotater.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragRotater : MonoBehaviour
+{
+    Vector3 posDelta = Vector3.zero;
+    Vector3 prevPos = Vector3.zero;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            posDelta = Input.mousePosition - prevPos;
+            if(Vector3.Dot(transform.up,Vector3.up) >= 0)
+            {
+                transform.Rotate(transform.up, -Vector3.Dot(posDelta, Camera.main.transform.right), Space.World);
+            }
+            else
+            {
+                transform.Rotate(transform.up, Vector3.Dot(posDelta, Camera.main.transform.right), Space.World);
+            }
+
+            transform.Rotate(Camera.main.transform.right, Vector3.Dot(posDelta, Camera.main.transform.up), Space.World);
+
+        }
+        prevPos = Input.mousePosition;
+    }
+}
