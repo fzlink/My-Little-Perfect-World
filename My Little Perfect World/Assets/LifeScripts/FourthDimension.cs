a0405edd44b3b9fccdd2a77d09f4e3fc4ab1bcd0
fzlink <fazlibozatay@gmail.com>
2020-02-29 10:04:42 +0300

    New things implemented

---
+++ My Little Perfect World/Assets/LifeScripts/FourthDimension.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FourthDimension : MonoBehaviour
+{
+    public enum TimeOfDay { Day, Night};
+    public TimeOfDay timeOfDay;
+    private float currentTime = 0;
+    private float currentDay = 0;
+    public float fullCycleTime = 200;
+    public float timeSpeedMultiplier = 1;
+
+    private void Start()
+    {
+        timeOfDay = TimeOfDay.Day;
+    }
+
+    private void Update()
+    {
+        currentTime += (Time.deltaTime / fullCycleTime) * timeSpeedMultiplier;
+        print(timeOfDay);
+        if(currentTime >= 0.5f)
+        {
+            timeOfDay = TimeOfDay.Night;
+        }
+        if(currentTime >= 1)
+        {
+            PassDay();
+        }
+    }
+
+    private void PassDay()
+    {
+        currentDay++;
+        currentTime = 0;
+        timeOfDay = TimeOfDay.Day;
+    }
+}
---
+++ My Little Perfect World/Assets/LifeScripts/Plants.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plants : MonoBehaviour
+{
+    public float chlorophyllAmount;
+    public float starch;
+    private Environment environment;
+    private FourthDimension time;
+    private Sun sun;
+    public float growFactor = 0.01f;
+
+    private void Start()
+    {
+        environment = FindObjectOfType<Environment>();
+        time = FindObjectOfType<FourthDimension>();
+        sun = FindObjectOfType<Sun>();
+    }
+
+    private void Update()
+    {
+        if(starch < 0)
+        {
+            starch = 0;
+        }
+
+        if(chlorophyllAmount > 0 && environment.carbondioxide > 0 && time.timeOfDay == FourthDimension.TimeOfDay.Day)
+        {
+            Photosynthesis();
+        }
+        else if(starch > 0 && time.timeOfDay == FourthDimension.TimeOfDay.Night)
+        {
+            Grow();
+        }
+    }
+
+    private void Photosynthesis()
+    {
+        starch += chlorophyllAmount * environment.carbondioxide * sun.sunlightStrength * Time.deltaTime;
+        environment.DecreaseCarbondioxide();
+        environment.IncreaseOxygen();
+    }
+
+    private void Grow()
+    {
+        Vector3 clampedScale = transform.localScale;
+        clampedScale += Vector3.one * starch * growFactor * Time.deltaTime;
+        clampedScale.x = Mathf.Clamp(clampedScale.x, 0, 1);
+        clampedScale.y = Mathf.Clamp(clampedScale.y, 0, 1);
+        clampedScale.z = Mathf.Clamp(clampedScale.z, 0, 1);
+        transform.localScale = clampedScale;
+
+        starch -= Time.deltaTime * growFactor;
+        environment.DecreaseOxygen();
+        environment.IncreaseCarbondioxide();
+    }
+
+}
